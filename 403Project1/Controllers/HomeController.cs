aeefc5f3278223c8880448d6679dd895b9ee2c65
Jakesaylor4 <55796475+Jakesaylor4@users.noreply.github.com>
2019-11-20 20:17:00 -0700

    Mission Page

--- 403Project1/Controllers/HomeController.cs
+++ 403Project1/Controllers/HomeController.cs
@@ -8,14 +8,34 @@ namespace _403Project1.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+
+    List<string> subjectList = new List<string>();
+
+    // GET: Home
+    public ActionResult Index()
         {
             return View();
         }
 
         public ActionResult Missions()
         {
+            List<SelectListItem> missionList = new List<SelectListItem>()
+            {
+                new SelectListItem
+                    {
+                        Text = "Portland Oregon", Value = "0"
+                    },
+                new SelectListItem
+                    {
+                        Text = "Seattle Washington", Value = "1"
+                    },
+                new SelectListItem
+                    {
+                        Text = "Los Angles California", Value = "2"
+                    }
+            };
+
+            ViewBag.MissionList = missionList;
             return View();
         }
         public ActionResult AboutUs()
@@ -25,6 +45,23 @@ namespace _403Project1.Controllers
 
         public ActionResult Contact()
         {
+            List<SelectListItem> subjectList = new List<SelectListItem>()
+            {
+                new SelectListItem
+                    {
+                        Text = "", Value = "0"
+                    },
+                new SelectListItem
+                    {
+                        Text = "Subject One", Value = "1"
+                    },
+                new SelectListItem
+                    {
+                        Text = "Subject Two", Value = "2"
+                    }
+            };
+
+            ViewBag.SelectList = subjectList;
             return View();
         }
     }
